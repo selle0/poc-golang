71df9b0ca12978a906f54833a8bec2643d99b74b
selle0 <gisellemezazavala17@gmail.com>
2024-02-28 23:54:03 -0700

    error arreglado

--- api.go
+++ api.go
@@ -7,36 +7,36 @@ import (
 	"net/http"
 )
 
-// func GetAllTasks(w http.ResponseWriter, r *http.Request) {
-// 	var tasks = getAllTasksRepository()
-// 	json.NewEncoder(w).Encode(tasks)
-// }
+func GetAllTasks(w http.ResponseWriter, r *http.Request) {
+	var tasks = getAllTasksRepository()
+	json.NewEncoder(w).Encode(tasks)
+}
 
-// func getTaskByID(w http.ResponseWriter, r *http.Request) {
-// 	params := mux.Vars(r)
-// 	taskID := 0
-// 	fmt.Sscanf(params["id"], "%d", &taskID)
-// 	var task = getTaskByIdRepository(taskID)
-// 	if task.ID > 0 {
-// 		json.NewEncoder(w).Encode(task)
-// 		return
-// 	}
-// 	json.NewEncoder(w).Encode(&Task{})
-// }
+func getTaskByID(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	taskID := 0
+	fmt.Sscanf(params["id"], "%d", &taskID)
+	var task = getTaskByIdRepository(taskID)
+	if task.ID > 0 {
+		json.NewEncoder(w).Encode(task)
+		return
+	}
+	json.NewEncoder(w).Encode(&Task{})
+}
 
-// func createTask(w http.ResponseWriter, r *http.Request) {
-// 	var newTask Task
-// 	_ = json.NewDecoder(r.Body).Decode(&newTask)
-// 	var task = createTaskRepository(newTask)
-// 	json.NewEncoder(w).Encode(task)
-// }
+func createTask(w http.ResponseWriter, r *http.Request) {
+	var newTask Task
+	_ = json.NewDecoder(r.Body).Decode(&newTask)
+	var task = createTaskRepository(newTask)
+	json.NewEncoder(w).Encode(task)
+}
 
 func updateTask(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	taskID := 0
 	fmt.Sscanf(params["id"], "%d", &taskID)
 	var updatedTask Task
-	// _ = json.NewDecoder(r.Body).Decode(&updatedTask)
+	_ = json.NewDecoder(r.Body).Decode(&updatedTask)
 	var task = updateTaskRepository(taskID, updatedTask)
 	if task.ID > 0 {
 		json.NewEncoder(w).Encode(task)
